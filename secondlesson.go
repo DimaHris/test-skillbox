7815444cd270c4a523b87e572e62b4f61cc66f1b
Дмитрий Хрисанфов <dmitr.khrisanfov@yandex.ru>
2021-12-06 21:04:55 +0300

    initial commit

---
+++ secondlesson.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func arrReverse(arr [10]int) int {
+	var x int
+	for i := 9; i >= 0; i-- {
+		fmt.Printf("%v ", arr[i])
+		x += arr[i]
+	}
+	return x
+
+}
+
+func main() {
+
+	var num [10]int
+
+	for i := 0; i < 10; i++ {
+		fmt.Scan(&num[i])
+	}
+
+	arrReverse(num)
+
+}
